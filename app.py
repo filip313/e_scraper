5970d097e8d335c07264adf1b975f505ea1b77a1
Filip <filip.ignjatovic12@gmail.com>
2021-11-06 17:35:40 +0100

    izmenjeno ucitavanje linkova i dodato preuzimanje html-a

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
 from urllib import request
 
 def ucitaj_linkove(putanja):
-    file = open(putanja, "rt")
-    linkovi = list()
-    for line in file:
-        linkovi.append(line.strip())
+	with open(putanja, 'rt') as f:
+		linkovi = []
+		for line in f:
+			linkovi.append(line.strip())
 
-    return linkovi
+		return linkovi
+   
+ 
+def preuzmi_sajt(link):
+	with request.urlopen(link) as page:
+		decoded_page = ''
+		for line in page:
+			line = line.decode('utf-8')
+			decoded_page += line
+
+		return decoded_page	
 
 
 putanja_do_linkova = "./linkovi.txt"
-
 linkovi = ucitaj_linkove(putanja_do_linkova)
+for link in linkovi:
+	page = preuzmi_sajt(link)
+	with open('page.html', 'w') as f:
+		f.write(page)
