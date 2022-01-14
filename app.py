c1555d01c0492df071d93219089f11ae2d96a40d
Filip <filip.ignjatovic12@gmail.com>
2022-01-14 17:47:17 +0100

    parsiranje i ispis :

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from urllib import request
+from bs4 import BeautifulSoup as soup
+import re
+from models import Cena, Linkovi, Proizvod, Prodavnica, MyEncoder
+from datetime import datetime
 
 def ucitaj_linkove(putanja):
 	with open(putanja, 'rt') as f:
@@ -18,10 +22,103 @@ def preuzmi_sajt(link):
 
 		return decoded_page	
 
+def parsiraj_podatke(page):
+	
+	product_name = ''
+	price = ''
+	seler = ''
+	cene = dict() 
+	linkovi = dict() 
+	prodavnice = dict()
+	parser = soup(page, 'html.parser')
+
+	for item in parser.find_all('script'):
+		name_list = re.search(r'var product_name = "(.*)";', str(item)) 
+		if name_list:
+			product_name = name_list.group(1)
+			break
+
+	for item in parser.find_all('tr'):
+		price = re.search(r'data-price="([0-9]*\.[0-9]*)" ', str(item))
+		seller = re.search(r'data-seller="([-a-zA-Z1-9 ]*)"', str(item))
+		seller_id = re.search(r'data-sellerid="([0-9]*)"', str(item))
+		store_link = re.search(r'href="([^"]*)"', str(item))
+		item_pos = re.search(r'data-position="([0-9]*)"', str(item))
+        
+		cena = Cena(0, -1, 0, 0)
+		link = Linkovi(0, "")
+		prodavnica = Prodavnica()
+		if price:
+            #print(price.group(1))
+			cena.cena = price.group(1)
+
+		if seller:
+            #print(seller.group(1))
+			prodavnica.ime = seller.group(1)
+
+		if seller_id:
+            #print(seller_id.group(1))        
+			prodavnica.id = seller_id.group(1)
+			prodavnice[prodavnica.id] = prodavnica
+			link.id_prod = seller_id.group(1)
+			cena.id_prod = seller_id.group(1)
+        
+		if store_link:
+            #print(store_link.group(1).replace('amp;', ''))
+			link.link = "https://www.eponuda.com" + store_link.group(1).replace('amp;', '')
+        
+		if item_pos:
+            #print(item_pos.group(1))
+			cena.id = len(cene.keys()) + 1
+			cena.datum = str(datetime.now().date())
+
+		flag = True 
+		if cena.id != -1:
+			for c in cene.values():
+				if c.id_prod == cena.id_prod and c.datum == cena.datum:
+					flag = False
+					break
+			if flag:
+				cene[cena.id] = cena
+			if link.id_prod not in linkovi.keys():
+				linkovi[link.id_prod] = link
+
+	proizvod = Proizvod(product_name, -1, cene, linkovi)
+
+	return prodavnice, proizvod
+
+def ispis(prodavnice, proizvodi):
+	for proizvod in proizvodi.values():
+		print(proizvod.ime)
+		print('----------------------------------------------')
+		prod_ids = []
+		for cena in proizvod.cene.values():
+			if cena.id_prod not in prod_ids:
+				prod_ids.append(cena.id_prod)
+
+		for prod_id in prod_ids:
+			print(f'\t{prodavnice[prod_id].ime}')
+			print(f'\t{proizvod.linkovi[prod_id].link}')
+			print(f'\t\tDATUM      | CENA')
+			for cena in proizvod.cene.values():
+				if cena.id_prod == prod_id:
+					print(f'\t\t{cena.datum} | {cena.cena}')
+					print('\t\t========================')
+		
+		print('\n')
+
 
 putanja_do_linkova = "./linkovi.txt"
 linkovi = ucitaj_linkove(putanja_do_linkova)
+proizvodi = dict()
+prodavnice = dict()
 for link in linkovi:
 	page = preuzmi_sajt(link)
-	with open('page.html', 'w') as f:
-		f.write(page)
+	prod, proi = parsiraj_podatke(page)
+	proi.id = len(proizvodi.keys()) + 1
+	proizvodi[proi.id] = proi
+	prodavnice.update(prod)
+
+encoder = MyEncoder()
+#print(encoder.encode(prodavnice))
+ispis(prodavnice, proizvodi)
--- soup_test.py
+++ soup_test.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup as soup
 import re
 from models import Cena, Linkovi, Proizvod, Prodavnica, MyEncoder
-import json
+from datetime import datetime
 
 product_name = ''
 price = ''
@@ -9,7 +9,7 @@ seler = ''
 
 cene = dict() 
 linkovi = dict() 
-prodavnica = Prodavnica()
+prodavnice = dict()
 
 with open('page.html', 'r') as page:
     parser = soup(page, 'html.parser')
@@ -21,14 +21,14 @@ with open('page.html', 'r') as page:
 
     for item in parser.find_all('tr'):
         price = re.search(r'data-price="([0-9]*\.[0-9]*)" ', str(item))
-        seller = re.search(r'data-seller="([-a-zA-Z1-9]*)"', str(item))
+        seller = re.search(r'data-seller="([-a-zA-Z1-9 ]*)"', str(item))
         seller_id = re.search(r'data-sellerid="([0-9]*)"', str(item))
         store_link = re.search(r'href="([^"]*)"', str(item))
         item_pos = re.search(r'data-position="([0-9]*)"', str(item))
         
         cena = Cena(0, -1, 0, 0)
         link = Linkovi(0, "")
-
+        prodavnica = Prodavnica()
         if price:
             #print(price.group(1))
             cena.cena = price.group(1)
@@ -40,18 +40,26 @@ with open('page.html', 'r') as page:
         if seller_id:
             #print(seller_id.group(1))        
             prodavnica.id = seller_id.group(1)
+            prodavnice[prodavnica.id] = prodavnica
             link.id_prod = seller_id.group(1)
             cena.id_prod = seller_id.group(1)
         
         if store_link:
             #print(store_link.group(1).replace('amp;', ''))
-            link.link = store_link.group(1).replace('amp;', '')
+            link.link = "https://www.eponuda.com" + store_link.group(1).replace('amp;', '')
         
         if item_pos:
             #print(item_pos.group(1))
-            cena.id = item_pos.group(1)
+            cena.id = len(cene.keys()) + 1
+            cena.datum = str(datetime.now().date())
+
+        flag = True 
         if cena.id != -1:
-            if cena.id not in cene.keys():
+            for c in cene.values():
+                if c.id_prod == cena.id_prod and c.datum == cena.datum:
+                    flag = False
+                    break
+            if flag:
                 cene[cena.id] = cena
             if link.id_prod not in linkovi.keys():
                 linkovi[link.id_prod] = link
@@ -63,4 +71,18 @@ proizvod.cene = cene
 proizvod.linkovi = linkovi
 #proizvod.print()
 #print(encoder.encode(cene))
-print(encoder.encode(proizvod))
+#print(encoder.encode(proizvod))
+#print(encoder.encode(prodavnice))
+print(proizvod.ime)
+print('----------------------------------------------')
+for prodavnica in prodavnice.values():
+    print(f'\t{prodavnica.ime}')
+    print(f'\t{proizvod.linkovi[prodavnica.id].link}')
+    print(f'\t\tDATUM      | CENA')
+    for cena in proizvod.cene.values():
+        if cena.id_prod == prodavnica.id:
+            print(f'\t\t{cena.datum} | {cena.cena}')
+            print('===================================================================')
+
+    print('-----------------------------------------------')
+        
